d862ec73256c1ec1393249948026a7cf136fc056
udendev <dennyaa2003@gmail.com>
2023-12-28 20:41:11 +0500

    feat: useTelegram hook

--- src/App.js
+++ src/App.js
@@ -1,17 +1,16 @@
 import './App.css';
 import { useEffect } from 'react';
-
-
+import { useTelegram } from './hooks/useTelegram';
 
 function App() {
+    const { tg, onToggleButton } = useTelegram();
     useEffect(() => {
         tg.ready();
     }, []);
 
-
     return (
         <div className="App">
-            work 
+            <button onClick={onToggleButton}>toggle</button>
         </div>
     );
 }
