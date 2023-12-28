caac56712d46d38b42e9067133ddfb03fd212a21
udendev <dennyaa2003@gmail.com>
2023-12-28 23:01:33 +0500

    fix: add dependency

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -13,7 +13,7 @@ const Form = () => {
         tg.MainButton.setParams({
             text: 'Отправить данные',
         });
-    }, []);
+    }, [tg]);
 
     useEffect(() => {
         if (!street || !country) {
