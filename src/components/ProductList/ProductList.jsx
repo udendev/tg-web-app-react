35433730110c94db3d652096da3e0d29d579f2c9
udendev <dennyaa2003@gmail.com>
2023-12-30 04:59:10 +0500

    fix: url

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -3,7 +3,7 @@ import ProductItem from '../ProductItem/ProductItem';
 import { useTelegram } from '../../hooks/useTelegram';
 import { useState, useCallback, useEffect } from 'react';
 
-const API_URL = 'http://89.111.174.107:8080/';
+const API_URL = 'http://89.111.174.107:8080';
 
 const products = [
     { id: '1', title: 'Джинсы', price: 5000, description: 'Синего цвета, прямые' },
@@ -29,7 +29,7 @@ const ProductList = () => {
             totalPrice: calcTotalPrice(addedItems),
         };
 
-        fetch(API_URL, {
+        fetch(API_URL + '/web-data', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
