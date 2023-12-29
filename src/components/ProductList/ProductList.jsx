f093066be393aa7dee1f1523e20d910102c55969
udendev <dennyaa2003@gmail.com>
2023-12-29 05:08:26 +0500

    fix: useState

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -1,6 +1,7 @@
 import './ProductList.css';
 import ProductItem from '../ProductItem/ProductItem';
 import { useTelegram } from '../../hooks/useTelegram';
+import { useState } from 'react';
 
 const products = [
     { id: '1', title: 'Джинсы', price: 5000, description: 'Синего цвета, прямые' },
